a4b16908990914802c11ce13ea0013f6d1c8bbff
penguang <penguang@penguangdeMacBook-Air.local>
2025-12-25 14:10:58 +0800

    Add database configuration file

---
+++ db_config.py
@@ -0,0 +1,10 @@
+MYSQL_INFO = {
+    "host": "localhost",    
+    "user": "root",         
+    "password": "!Penguang12345",         
+    "database": "web_pipeline", 
+    "port": 3306           
+}
+
+
+TITLE = "个人资产负债表可视化"
